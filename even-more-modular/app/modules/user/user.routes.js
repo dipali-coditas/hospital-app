ae3861051d1705e8147f268438bf2f0924b51a88
Aniruddha Gohad <aniruddha@radontechsolutions.com>
2022-02-15 11:51:47 +0530

    assignment code

--- even-more-modular/app/modules/user/user.routes.js
+++ even-more-modular/app/modules/user/user.routes.js
@@ -1,20 +1,30 @@
 import { Router } from "express";
 import UserDB from './user.schema.js';
+import { CreateUserValidator } from './user.validations.js'
 
 const router = Router();
 
+// validate incoming data
+// handling errors
 
-router.post("/", (req, res, next) => {
-    res.send('user post api');
+router.post("/", CreateUserValidator(), (req, res, next) => {
+    try {
+        const result = UserDB.save(req.body);
+        res.send({ message: "USER CREATED", data: result });
+    } catch (e) {
+        res.status(500).send({ message: 'SOMETHING WENT WRONG' });
+    }
 });
 
+
+// validate the get request
 router.get("/", (req, res, next) => {
     try {
         const result = UserDB.getAll();
         // success result
         // by default the statusCode will be
         // 200 -> success
-        res.send(result);
+        res.send({ message: "FETCHED", data: result });
     } catch (e) {
         // not the success result
         // we set the statusCode to some error code
@@ -23,12 +33,25 @@ router.get("/", (req, res, next) => {
     }
 });
 
+// validate the put request
 router.put("/", (req, res, next) => {
-    res.send('user put api');
+    try {
+        UserDB.updateOne(req.body);
+        res.send({ message: "USER UPDATED", data: req.body });
+    } catch (e) {
+        res.status(500).send({ message: 'SOMETHING WENT WRONG' });
+    }
 });
 
-router.delete("/", (req, res, next) => {
-    res.send('user delete api');
+// validate the delete request
+router.delete("/:id", (req, res, next) => {
+    try {
+        const { id } = req.params;
+        UserDB.deleteOne(id);
+        res.send({ message: "USER DELETED", data: null });
+    } catch (e) {
+        res.status(500).send({ message: 'SOMETHING WENT WRONG' });
+    }
 });
 
 export default router;
--- even-more-modular/app/modules/user/user.schema.js
+++ even-more-modular/app/modules/user/user.schema.js
@@ -3,13 +3,18 @@
 // implement the save, updateOne & deleteOne methods
 // send the user.schema.js file via DM on slack
 // use these methods from the user.routes.js file
+import { v4 } from 'uuid';
 
 class UserSchema {
     users = [];
 
     // methods
     save(user) {
+        const id = v4();
+        const userRecord = { ...user, id };
+        this.users.push(userRecord);
 
+        return userRecord; 
     }
 
     getAll() {
@@ -18,11 +23,23 @@ class UserSchema {
     }
 
     updateOne(user) {
+        const userRecord = this.users.find(u => u.id === user.id);
 
+        if(!userRecord) throw { message: 'USER NOT FOUND' };
+
+        userRecord = user;
+
+        return true;
     }
 
     deleteOne(userId) {
+        const userIndex = this.users.findIndex(u => u.id === userId);
 
+        if(userIndex < 0) throw { message: 'USER NOT FOUND' };
+
+        this.users.splice(userIndex, 1);
+
+        return true;
     }
 }
 
